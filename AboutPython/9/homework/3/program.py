323dd002b8e0c043cc04267a4a0a86a72ad5fc85
garrip91 <garrip91@gmail.com>
2023-08-21 13:11:20 +0300

    Add program.py about workers

---
+++ AboutPython/9/homework/3/program.py
@@ -0,0 +1,25 @@
+wage = float(input("Введите размер оклада сотрудника:\n"))
+bonus = float(input("Введите размер премии сотрудника:\n"))
+
+
+class Worker:
+
+    def __init__(self, name, surname, position, income={"wage": wage, "bonus": bonus}):
+        self.name = name,
+        self.surname = surname
+        self.position = position
+        self._income = income
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+    
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+position = Position("Сергей", "Гребенщиков", "кладовщик")
+print(position.get_full_name())
+print(position.get_total_income())
