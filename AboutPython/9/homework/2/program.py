e73ce58211369a08937526ec74a7f564dda8e06c
garrip91 <garrip91@gmail.com>
2023-08-21 12:26:55 +0300

    Add program.py about road

--- AboutPython/9/homework/2/program.py
+++ AboutPython/9/homework/2/program.py
@@ -1,1 +1,12 @@
-#
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+    
+    def mass_of_asphalt(self):
+        return self._length * self._width
+
+
+road = Road(7, 8)
+print(road.mass_of_asphalt())
